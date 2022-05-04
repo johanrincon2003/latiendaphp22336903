803cc358bf52fe94be03f86ed0d1021edf43e460
John <you@example.com>
2022-05-04 17:16:23 -0500

    Migraciones y materialize

--- resources/views/layouts/principal.blade.php
+++ resources/views/layouts/principal.blade.php
@@ -15,7 +15,7 @@
       <ul id="nav-mobile" class="right hide-on-med-and-down">
         <li><a href="sass.html">Sass</a></li>
         <li><a href="badges.html">Hola mundo</a></li>
-        <li><a href="collapsible.html">JavaScript</a></li>
+        <li><a href="collapsible.html">jhojan paspis</a></li>
       </ul>
     </div>
   </nav>
