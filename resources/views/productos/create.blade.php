c3897949b51e631e08d94101f5c2237b326dcb8f
John <you@example.com>
2022-05-11 15:24:45 -0500

    Adaptando vista

--- resources/views/productos/create.blade.php
+++ resources/views/productos/create.blade.php
@@ -11,39 +11,41 @@
     <form class="col s12">
       <div class="row">
         <div class="input-field col s8">
-          <input placeholder="Placeholder" id="first_name" type="text" class="validate">
-          <label for="first_name"></label>
-        </div>
-        <div class="input-field col s6">
-          <input id="last_name" type="text" class="validate">
-          <label for="last_name">Last Name</label>
+          <input id="nombre" name="nombre" type="text" class="validate">
+          <label for="nombre">Nompre de producto de la tienda de Jean</label>
         </div>
       </div>
       <div class="row">
-        <div class="input-field col s12">
-          <input disabled value="I am not editable" id="disabled" type="text" class="validate">
-          <label for="disabled">Disabled</label>
+        <div class="input-field col s8">
+          <textarea id="desc" class="materialize-textarea" name="desc"></textarea>
+          <label for="textarea1">Descripcion  </label>
+          <label for="Descripcion"></label>
         </div>
       </div>
       <div class="row">
-        <div class="input-field col s12">
-          <input id="password" type="password" class="validate">
-          <label for="password">Password</label>
+        <div class="input-field col s8">
+          <input id="precio" type="number" class="validate" name="precio">
+          <label for="precio">Precio</label>
         </div>
       </div>
       <div class="row">
-        <div class="input-field col s12">
-          <input id="email" type="email" class="validate">
-          <label for="email">Email</label>
+        <div class="input-field col s8">
+          <div class="file-field input-field">
+            <div class="btn">
+              <span>Imagen de producto</span>
+              <input type="file" name="imagen">
+            </div>
+            <div class="file-path-wrapper">
+              <input class="file-path validate" type="text">
+            </div>
+          </div>
         </div>
       </div>
       <div class="row">
-        <div class="col s12">
-          This is an inline input field:
-          <div class="input-field inline">
-            <input id="email_inline" type="email" class="validate">
-            <label for="email_inline">Email</label>
-            <span class="helper-text" data-error="wrong" data-success="right">Helper text</span>
+        <div class="col s8">
+          <button class="btn waves-effect waves-light" type="submit" name="action">Guardar
+            <i class="material-icons right"></i>
+          </button>
           </div>
         </div>
       </div>
