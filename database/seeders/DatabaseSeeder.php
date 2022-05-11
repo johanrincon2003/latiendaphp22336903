580dcab002c3cbc1342d6a9f2e924c651ed3b141
John <you@example.com>
2022-05-11 14:11:07 -0500

    Creando el controlador de producto

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,7 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-
+use App\Models\Categoria;
+use App\Models\Marca;
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -15,6 +16,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
-        
+        Categoria::factory(10)->create();
+        Marca::factory(10)->create();        
     }
 }
