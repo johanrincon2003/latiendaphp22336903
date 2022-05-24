14de2fe7536b084b01020d8f6687b96f4af74c05
Your Name <you@example.com>
2022-05-24 14:22:25 -0500

    evidencia_02

--- resources/views/productos/create.blade.php
+++ resources/views/productos/create.blade.php
@@ -1,81 +1,99 @@
 @extends('layouts.principal')
 
-@section('contenido')
-<div class="row"> 
-  <h2 class="cyan-text text-accent-2">Nuevo Producto del Lichigo de Jean</h2>
+@section ('contenido')
+<div class="row">
+  <h1 class=" grey-text text-darken-1">Nuevo Producto</h1>
 </div>
-    
-  
-  
-  <div class="row">
-    <form method="POST"   action="{{route('productos.store')}}"
-     class="col s12">
-     @csrf  
-      <div class="row">
-        <div class="input-field col s8">
-          <input id="nombre" name="nombre" type="text" class="validate">
-          <label for="nombre">Nompre de producto de la tienda de Jean</label>
-        </div>
-      </div>
-      <div class="row">
-        <div class="input-field col s8">
-          <textarea id="desc" class="materialize-textarea" name="desc"></textarea>
-        
-          <label for="desc">Descripcion  </label>         
-        </div>
-      </div>
-      <div class="row">
-        <div class="input-field col s8">
-          <input id="precio" type="number" class="validate" name="precio">
-          <label for="precio">Precio</label>
-        </div>
-      </div>
-      <div class="row">
-        <div class="input-field col s8">
-          <div class="file-field input-field">
-            <div class="btn">
-              <span>Imagen de producto</span>
-              <input type="file" name="imagen">
-            </div>         
-          </div>
-        </div>
-      </div>
-      <div class="row">
-        <div class="input-field col s8 ">
-          <select name="categoria">
-            <option value="" disabled selected>Eliga Categoria</option>
-            @foreach ($categorias as $categoria)
-              <option value="{{$categoria->id}}">{{$categoria->nombre}}</option>
-
-            @endforeach
-          </select>
-          <label>Categorias disponibles en la tienda del Jean</label>
-        </div>
-      </div>
-    
-      <div class="row">
-        <div class="input-field col s8 ">
-          <select name="marca">
-            <option value="" disabled selected>Eliga marca</option>
-            @foreach ($marcas as $marca)
-              <option value="{{$marca->id}}">{{$marca->nombre}}</option>
-
-            @endforeach
-          </select>
-          <label>Categorias disponibles en la tienda del Jean</label>
-        </div>
-      </div>
-
-
+<div class="row">
+    <form method="POST"
+       action="{{route('productos.store') }}"
+       class="col s12">
+    @csrf  
+    @if(session('mensaje'))
       <div class="row">
         <div class="col s8">
-          <button class="btn waves-effect waves-light" type="submit" name="action">Guardar
-            <i class="material-icons right"></i>
-          </button>
-          </div>
+          <span class="teal-text text-darken-2">
+            {{  session('mensaje')  }}
+          </span>
         </div>
+      </div>
+    @endif
+    
+      <div class="row">
+        <div class="input-field col s8">
+          <input placeholder="Nombre de Producto" 
+          id="nombre" 
+          type="text" 
+          class="validate"
+          name="nombre">
+          <label for="Nombre">Nombre de Producto</label>
+          <span> {{ $errors->first('nombre')}}</span>
+        </div>
+      </div>
+      <div class="row">
+        <div class="input-field col s8">
+          <textarea class="materialize-textarea"
+             id="descripcion"
+             name="descripcion">
+          </textarea>
+          <label for="descripcion">Descripcion</label>
+          <span> {{ $errors->first('descripcion')}}</span>
+        </div>
+      </div>
+      <div class="row">
+        <div class="input-field col s8">
+          <input placeholder=""
+          id="precio"
+          name="precio" 
+          type="text"  
+          class="validate">
+          <label for="Precio">Precio</label>
+          <span> {{ $errors->first('precio')}}</span>
+        </div>
+        <div class="file-field input-field col s8">
+      <div class="btn">
+        <span>Imagen de Producto</span>
+        <input type="file" name="imagen">
+      </div>
+      <div class="file-path-wrapper">
+        <input class="file-path validate" type="text">
+      </div>
+    </div>
+      </div>
+      <div class="row">
+      <div class="input-field col s8">
+      <select name="categoria">
+      <option value="" disabled selected>Elija categoria</option>
+      @foreach($categorias as $categoria)
+      <option value="{{ $categoria->id}}">
+        {{ $categoria->nombre}}
+      </option>
+      @endforeach
+    </select>
+    <label>Categorias Disponibles</label>
+    <span> {{ $errors->first('categoria')}}</span>
+      </div>
+      </div>
+      <div class="row">
+      <div class="input-field col s8">
+      <select name="Marca">
+      <option value="" disabled selected>Elija Marca</option>
+      @foreach($marcas as $marca)
+      <option value="{{ $marca->id}}">
+        {{ $marca->nombre}}
+      </option>
+      @endforeach
+    </select>
+    <label>Marcas Disponibles</label>
+    <span> {{ $errors->first('Marca')}}</span>
+      </div>
+      </div>
+      <div class="row">
+      <button class="btn waves-effect waves-light"
+       type="submit" 
+       >Guardar
+      </button>
       </div>
     </form>
   </div>
-        
-@endsection
+  @endsection
